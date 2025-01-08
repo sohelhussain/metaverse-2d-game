e02560440d880bdabd7ba99902ca279fa017b833
sohelhussain <sohelhussaing@gmail.com>
2025-01-08 09:36:08 +0530

    test: WRITING TEST FOR AUTHENTICATION

---
+++ test/index.test.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+
+function sum(a, b) {
+  return a + b;
+}
+
+const BACKEND_URL = "http://localhost:3000";
+
+describe("Authentication", () => {
+  test("User is able to signup only once", async () => {
+    const username = "sohel" + Math.random();
+    const password = "123456";
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+    expect(response.statusCode).toBe(200);
+    const updateResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+    expect(updateResponse.statusCode).toBe(400);
+  });
+  test("Signup request fails if the username is empty", async () => {
+    const password = "123456";
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      password,
+      type: "admin",
+    });
+    expected(response.statusCode).toBe(400);
+  });
+  test("Signin succeed if the username and password are correct", async () => {
+    const username = `sohel-${Math.random()}`;
+    const password = "132456";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+    expected(response.statusCode).toBe(200);
+    expected(response.body.token).toBeDefined();
+  });
+  test("Signin fail if the username and password are incorrect", async () => {
+    const username = `sohel-${Math.random()}`;
+    const password = "132456";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username: 'alsdfkj',
+      password,
+    });
+    expected(response.statusCode).toBe(403);
+    expected(response.body.token).toBeUndefined();
+  });
+});
